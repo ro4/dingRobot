c667ec92735ebdc7cb1a90d1144e4cfd02c8724c
ro4 <thefrp@gmail.com>
2017-11-15 16:17:41 +0800

    markdown

--- src/DingRobot/Message/Markdown.php
+++ src/DingRobot/Message/Markdown.php
@@ -4,5 +4,20 @@ namespace DingRobot\Message;
 
 class Markdown extends Base
 {
+    public function __construct($title = '')
+    {
+        parent::__construct();
+        $this->body['title'] = $title;
+        $this->obj           = 'markdown';
+    }
 
+    function getArray()
+    {
+        return $this->body;
+    }
+
+    function bodyFields()
+    {
+        return ['title', 'text'];
+    }
 }
